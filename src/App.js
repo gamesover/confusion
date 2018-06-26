ebcf7d04c1a5dea47c35c0c5eae477b7ae457a23
Ken <ken.ding@mail.com>
2018-06-27 05:34:50 +0930

    React Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {Navbar, NavbarBrand} from 'reactstrap'
+import logo from './logo.svg'
+import './App.css'
 
-import { DISHES } from './shared/dishes';
-import Menu from './components/MenuComponent';
-import Main from './components/MainComponent';
+import {DISHES} from './shared/dishes'
+import Menu from './components/MenuComponent'
+import Main from './components/MainComponent'
+import {BrowserRouter} from 'react-router-dom'
 
 class App extends Component {
-
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
-    );
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default App
